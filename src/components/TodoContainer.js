d11d4f59f95a64e511884c6dce6752c11f360144
ridwan <ridwanediallo99@gmail.com>
2022-04-26 12:33:47 +0000

    How to add CSS in Reactjs App

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -60,17 +60,19 @@ class TodoContainer extends React.Component {
   };
 
   render() {
-    return (
-      <div>
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-        />
-      </div>
-    );
+   return (
+     <div className="container">
+       <div className="inner">
+         <Header />
+         <InputTodo addTodoProps={this.addTodoItem} />
+         <TodosList
+           todos={this.state.todos}
+           handleChangeProps={this.handleChange}
+           deleteTodoProps={this.delTodo}
+         />
+       </div>
+     </div>
+   );
   }
 }
 export default TodoContainer;
