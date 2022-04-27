a7755b773e82ece9caf0e55794581ed0e8b66cad
ridwan <ridwanediallo99@gmail.com>
2022-04-27 21:30:37 +0000

    How to Edit Todos Items in a React Application

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -59,20 +59,32 @@ class TodoContainer extends React.Component {
     });
   };
 
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      }),
+    });
+  };
+
   render() {
-   return (
-     <div className="container">
-       <div className="inner">
-         <Header />
-         <InputTodo addTodoProps={this.addTodoItem} />
-         <TodosList
-           todos={this.state.todos}
-           handleChangeProps={this.handleChange}
-           deleteTodoProps={this.delTodo}
-         />
-       </div>
-     </div>
-   );
+    return (
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+            setUpdate={this.setUpdate}
+          />
+        </div>
+      </div>
+    );
   }
 }
 export default TodoContainer;
